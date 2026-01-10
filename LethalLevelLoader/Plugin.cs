14cb8813082b7a49ec4abfd9d30f7ae7d82d85bb
pacoito123 <pacoito@protonmail.com>
2026-01-10 03:58:27 -0600

    Release v1.6.3

--- LethalLevelLoader/Plugin.cs
+++ LethalLevelLoader/Plugin.cs
@@ -18,7 +18,7 @@ namespace LethalLevelLoader
     {
         public const string ModGUID = "imabatby.lethallevelloader";
         public const string ModName = "LethalLevelLoader";
-        public const string ModVersion = "1.6.2";
+        public const string ModVersion = "1.6.3";
 
         internal static Plugin Instance;
 
