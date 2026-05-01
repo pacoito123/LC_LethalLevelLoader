0a2574092ffc7927d57f8309c77a730620dec114
pacoito123 <pacoito@protonmail.com>
2026-05-01 01:58:18 -0600

    Very smol reformatting.

--- LethalLevelLoader/Components/ExtendedContent/ExtendedFootstepSurface.cs
+++ LethalLevelLoader/Components/ExtendedContent/ExtendedFootstepSurface.cs
@@ -2,95 +2,96 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace LethalLevelLoader;
-
-[CreateAssetMenu(fileName = "ExtendedFootstepSurface", menuName = "Lethal Level Loader/Extended Content/ExtendedFootstepSurface", order = 27)]
-public class ExtendedFootstepSurface : ExtendedContent
+namespace LethalLevelLoader
 {
-    [field: Header("General Settings")]
-    [field: SerializeField] public FootstepSurface FootstepSurface { get; set; }
-
-    [field: Header("Extended Feature Settings")]
-    [field: SerializeField] public List<TerrainWithIndices> AssociatedTerrains { get; set; } = [];
-    [field: SerializeField] public bool AllowSinking { get; set; } = true;
-
-    [HideInInspector] public Dictionary<string, byte> AssociatedTerrainNames { get; } = [];
-    [HideInInspector] public int SurfaceIndex { get; internal set; } = -1;
-
-    [Space(25)]
-    [Header("Obsolete (Legacy Fields, Will Be Removed In The Future)")]
-    [Obsolete] public FootstepSurface footstepSurface;
-    [Obsolete] public List<Material> associatedMaterials;
-
-    internal static ExtendedFootstepSurface Create(FootstepSurface newFootstepSurface, params TerrainWithIndices[] newAssociatedTerrains)
+    [CreateAssetMenu(fileName = "ExtendedFootstepSurface", menuName = "Lethal Level Loader/Extended Content/ExtendedFootstepSurface", order = 27)]
+    public class ExtendedFootstepSurface : ExtendedContent
     {
-        ExtendedFootstepSurface newExtendedFootstepSurface = ScriptableObject.CreateInstance<ExtendedFootstepSurface>();
-        newExtendedFootstepSurface.FootstepSurface = newFootstepSurface;
+        [field: Header("General Settings")]
+        [field: SerializeField] public FootstepSurface FootstepSurface { get; set; }
 
-        if (newAssociatedTerrains != null)
-            newExtendedFootstepSurface.AssociatedTerrains = [.. newAssociatedTerrains];
-        return (newExtendedFootstepSurface);
-    }
+        [field: Header("Extended Feature Settings")]
+        [field: SerializeField] public List<TerrainWithIndices> AssociatedTerrains { get; set; } = [];
+        [field: SerializeField] public bool AllowSinking { get; set; } = true;
 
-    internal void Initialize()
-    {
-        name = FootstepSurface.surfaceTag.Sanitized(toLower: false) + "ExtendedFootstepSurface";
+        [HideInInspector] public Dictionary<string, byte> AssociatedTerrainNames { get; } = [];
+        [HideInInspector] public int SurfaceIndex { get; internal set; } = -1;
 
-        if (ContentType is ContentType.Custom)
+        [Space(25)]
+        [Header("Obsolete (Legacy Fields, Will Be Removed In The Future)")]
+        [Obsolete] public FootstepSurface footstepSurface;
+        [Obsolete] public List<Material> associatedMaterials;
+
+        internal static ExtendedFootstepSurface Create(FootstepSurface newFootstepSurface, params TerrainWithIndices[] newAssociatedTerrains)
         {
-            string identifier = (ExtendedMod.ModMergeSetting) switch
-            {
-                ModMergeSetting.MatchingAuthorName => ExtendedMod.AuthorName,
-                ModMergeSetting.MatchingModName => ExtendedMod.ModName,
-                _ => UniqueIdentificationName
-            };
+            ExtendedFootstepSurface newExtendedFootstepSurface = ScriptableObject.CreateInstance<ExtendedFootstepSurface>();
+            newExtendedFootstepSurface.FootstepSurface = newFootstepSurface;
 
-            FootstepSurface.surfaceTag = identifier + '/' + FootstepSurface.surfaceTag;
+            if (newAssociatedTerrains != null)
+                newExtendedFootstepSurface.AssociatedTerrains = [.. newAssociatedTerrains];
+            return (newExtendedFootstepSurface);
+        }
+
+        internal void Initialize()
+        {
+            name = FootstepSurface.surfaceTag.Sanitized(toLower: false) + "ExtendedFootstepSurface";
+
+            if (ContentType is ContentType.Custom)
+            {
+                string identifier = (ExtendedMod.ModMergeSetting) switch
+                {
+                    ModMergeSetting.MatchingAuthorName => ExtendedMod.AuthorName,
+                    ModMergeSetting.MatchingModName => ExtendedMod.ModName,
+                    _ => UniqueIdentificationName
+                };
+
+                FootstepSurface.surfaceTag = identifier + '/' + FootstepSurface.surfaceTag;
+            }
+        }
+
+        internal void RefreshAssociatedTerrainNames()
+        {
+            AssociatedTerrainNames.Clear();
+            foreach (TerrainWithIndices associatedTerrain in AssociatedTerrains)
+                if (!AssociatedTerrainNames.TryAdd(associatedTerrain.terrainName, associatedTerrain.GetIndexMask())) // TODO: Handle duplicate names better...
+                    DebugHelper.LogWarning($"Terrain name {associatedTerrain.terrainName} registered more than once in FootstepSurface: {name}", DebugType.Developer);
+        }
+
+        internal bool IsTerrainMatch(TerrainData terrainData) => AssociatedTerrainNames.ContainsKey(terrainData.name);
+        internal bool IsTerrainMatch(TerrainData terrainData, int terrainLayer) => AssociatedTerrainNames.TryGetValue(terrainData.name, out byte terrainMask) && ((byte)(1 << terrainLayer) & terrainMask) != 0;
+
+        internal override (bool result, string log) TryValidateContent()
+        {
+            if (FootstepSurface == null)
+                return ((false, "FootstepSurface Was Null"));
+
+            // TODO: Some more validation.
+
+            if (string.IsNullOrEmpty(FootstepSurface.surfaceTag))
+                FootstepSurface.surfaceTag = "Untagged";
+            return (base.TryValidateContent());
         }
     }
 
-    internal void RefreshAssociatedTerrainNames()
+    [Serializable]
+    public struct TerrainWithIndices(string terrainName, params int[] layerIndices)
     {
-        AssociatedTerrainNames.Clear();
-        foreach (TerrainWithIndices associatedTerrain in AssociatedTerrains)
-            if (!AssociatedTerrainNames.TryAdd(associatedTerrain.terrainName, associatedTerrain.GetIndexMask())) // TODO: Handle duplicate names better...
-                DebugHelper.LogWarning($"Terrain name {associatedTerrain.terrainName} registered more than once in FootstepSurface: {name}", DebugType.Developer);
-    }
+        public string terrainName = terrainName;
+        [Range(0, 7)]
+        public int[] layerIndices = layerIndices;
 
-    internal bool IsTerrainMatch(TerrainData terrainData) => AssociatedTerrainNames.ContainsKey(terrainData.name);
-    internal bool IsTerrainMatch(TerrainData terrainData, int terrainLayer) => AssociatedTerrainNames.TryGetValue(terrainData.name, out byte terrainMask) && ((byte)(1 << terrainLayer) & terrainMask) != 0;
+        public readonly byte GetIndexMask()
+        {
+            HashSet<int> indices = [.. layerIndices];
+            int removedIndices = indices.RemoveWhere(index => index < 0 || index > 7);
+            if (removedIndices > 0)
+                DebugHelper.LogWarning($"Removed {removedIndices} indices for Terrain: {terrainName}", DebugType.Developer);
 
-    internal override (bool result, string log) TryValidateContent()
-    {
-        if (FootstepSurface == null)
-            return ((false, "FootstepSurface Was Null"));
+            byte indexMask = 0;
+            foreach (int index in indices)
+                indexMask += (byte)(1 << index);
 
-        // TODO: Some more validation.
-
-        if (string.IsNullOrEmpty(FootstepSurface.surfaceTag))
-            FootstepSurface.surfaceTag = "Untagged";
-        return (base.TryValidateContent());
-    }
-}
-
-[Serializable]
-public struct TerrainWithIndices(string terrainName, params int[] layerIndices)
-{
-    public string terrainName = terrainName;
-    [Range(0, 7)]
-    public int[] layerIndices = layerIndices;
-
-    public readonly byte GetIndexMask()
-    {
-        HashSet<int> indices = [.. layerIndices];
-        int removedIndices = indices.RemoveWhere(index => index < 0 || index > 7);
-        if (removedIndices > 0)
-            DebugHelper.LogWarning($"Removed {removedIndices} indices for Terrain: {terrainName}", DebugType.Developer);
-
-        byte indexMask = 0;
-        foreach (int index in indices)
-            indexMask += (byte)(1 << index);
-
-        return indexMask;
+            return indexMask;
+        }
     }
 }
