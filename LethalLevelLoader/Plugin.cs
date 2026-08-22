b4abe577134a13bcdd6fbd9f8a9cb1845e867d98
pacoito123 <pacoito@protonmail.com>
2026-08-21 20:21:16 -0600

    Release v1.7.11

--- LethalLevelLoader/Plugin.cs
+++ LethalLevelLoader/Plugin.cs
@@ -16,7 +16,7 @@ namespace LethalLevelLoader
     {
         public const string ModGUID = "imabatby.lethallevelloader";
         public const string ModName = "LethalLevelLoader";
-        public const string ModVersion = "1.7.10";
+        public const string ModVersion = "1.7.11";
 
         internal static readonly Harmony Harmony = new Harmony(ModGUID);
         internal static new readonly ManualLogSource Logger = BepInEx.Logging.Logger.CreateLogSource(ModName);
